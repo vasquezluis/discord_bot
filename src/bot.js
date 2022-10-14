520160b2cdf3fcd591319cc74df05fc48dc8cf33
LuiiVasquez <luivasque95@gmail.com>
2022-10-13 23:59:37 -0600

    Listening and handling events

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,39 @@
-import { config } from 'dotenv';
-import { Client} from "discord.js";
+import { config } from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
 
 config();
 
-const client = new Client({ intents: ['Guilds', 'GuildMessages'] });
-const TOKEN = process.env.DISCORD_TOKEN
+// create the cliente
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
+const TOKEN = process.env.DISCORD_TOKEN;
+
+// cliente initialization
 client.login(TOKEN);
+
+// listen to an event
+client.on("ready", () => {
+  console.log(`${client.user.tag} has logged in.`);
+});
+
+// handle message event
+client.on("messageCreate", (message) => {
+  console.log(message.content);
+  console.log(message.createdAt.toDateString());
+  console.log(message.author.tag);
+});
+
+// handle a create channel event
+client.on("channelCreate", (channel) => {
+  console.log(channel.name);
+  console.log(channel.createdAt);
+  console.log(channel.id);
+})
+
+
